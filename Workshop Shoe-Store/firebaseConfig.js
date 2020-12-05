dc6012e50e5e0f6194df266744e84ded05b565fc
Yordan Krushkov <yordankrushkov@gmail.com>
2020-12-05 17:13:17 +0000

    login and logout

---
+++ Workshop Shoe-Store/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyAe_hG_ac1t_HwMtYiT2eUQksy9ahqR3k4",
+    authDomain: "shoe-shelf-824a4.firebaseapp.com",
+    databaseURL: "https://shoe-shelf-824a4-default-rtdb.firebaseio.com",
+    projectId: "shoe-shelf-824a4",
+    storageBucket: "shoe-shelf-824a4.appspot.com",
+    messagingSenderId: "811763867669",
+    appId: "1:811763867669:web:74f167cd5f15bd6554ee1c"
+  };
+ 
+  firebase.initializeApp(firebaseConfig);
--- Workshop Shoe-Store/index.js
+++ Workshop Shoe-Store/index.js
@@ -1,100 +1,108 @@
 const userModel = firebase.auth();
 
 
-const app = Sammy('#root', function(){
+const app = Sammy('#root', function () {
 
     this.use('Handlebars', 'hbs');
-    this.get('/home', function(context){
+    this.get('/home', function (context) {
         extendContent(context)
-        .then(function(){
-            this.partial('./templates/homeGuest.hbs');
-        })
+            .then(function () {
+                this.partial('./templates/homeGuest.hbs');
+            })
     });
 
-    this.get('/register',function(context){
+    this.get('/register', function (context) {
         extendContent(context)
-        .then(function(){
-            this.partial('./templates/register.hbs');
-        })
+            .then(function () {
+                this.partial('./templates/register.hbs');
+            })
     });
 
-    this.post('/register',function(context){
-        const {email, password, rePassword}=context.params;
+    this.post('/register', function (context) {
+        const { email, password, rePassword } = context.params;
 
-        if( password!==rePassword && password.length<6){
+        if (password !== rePassword && password.length < 6) {
             return;
         }
-        userModel.createUserWithEmailAndPassword(email,password)
-        .then((userData)=>{
-            this.redirect('/login')
-        })
-        .catch((error)=>console.log(error))
+        userModel.createUserWithEmailAndPassword(email, password)
+            .then((userData) => {
+                this.redirect('/login')
+            })
+            .catch((error) => console.log(error))
     })
 
-    this.get('/login',function(context){
+    this.get('/login', function (context) {
         extendContent(context)
-        .then(function(){
-            this.partial('./templates/login.hbs');
-        })
+            .then(function () {
+                this.partial('./templates/login.hbs');
+            })
 
     });
 
-    this.post('/login',function(context){
-        const {email, password}=context.params;
+    this.post('/login', function (context) {
+        const { email, password } = context.params;
 
         userModel.signInWithEmailAndPassword(email, password)
-        .then((userData)=>{
-           saveUserData(userData);
-            this.redirect('/home')
-        })
-        .catch((error)=>console.log(error))
+            .then((userData) => {
+                saveUserData(userData);
+                this.redirect('/home')
+            })
+            .catch((error) => console.log(error))
     })
-   
-    this.get('/details',function(context){
+
+    this.get('/details', function (context) {
 
         extendContent(context)
-        .then(function(){
-            this.partial('./templates/details.hbs')
-        })
-        
+            .then(function () {
+                this.partial('./templates/details.hbs')
+            })
+
     });
- 
-    this.get('/edit-offer',function(context){
+
+    this.get('/edit-offer', function (context) {
 
         extendContent(context)
-        .then(function(){
-            this.partial('./templates/editForm.hbs')
-        })
-        
+            .then(function () {
+                this.partial('./templates/editForm.hbs')
+            })
+
     });
-   
-    this.get('/create-offer',function(context){
+
+    this.get('/create-offer', function (context) {
         extendContent(context)
-        .then(function(){
-            this.partial('./templates/newOffer.hbs')
-        })
-       
+            .then(function () {
+                this.partial('./templates/newOffer.hbs')
+            })
+
     });
+
+    this.get('/logout', function(context){
+        localStorage.removeItem('user');
+        this.redirect('/home')
+    })
 });
 
 
-(()=>{
+(() => {
     app.run('#/home');
 })();
 
-function extendContent(context){
-  return  context.loadPartials({
-        'header':'./partials/header.hbs',
+function extendContent(context) {
+    const user = getUserData();
+    context.isLoggedIn = Boolean(user);
+    context.email = user ? user.email : '';
+    return context.loadPartials({
+        'header': './partials/header.hbs',
         'footer': './partials/footer.hbs'
     })
 }
 
-function saveUserData(data){
-    const {user: {email, uid}}=data;
-    localStorage.setItem('user', JSON.stringify({email, uid}))
+function saveUserData(data) {
+    const { user: { email, uid } } = data;
+    localStorage.setItem('user', JSON.stringify({ email, uid }))
 }
 
-function getUserData(){
-    const user= localStorage.getItem('user');
-    return user? JSON.parse(user):null;
+function getUserData() {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : null;
 }
