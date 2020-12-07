ad92bd1f3ca3fbd31933e1425f3607d0db952e22
Yordan Krushkov <yordankrushkov@gmail.com>
2020-12-07 09:41:36 +0000

    not found page

--- Workshop Soft Wiki/app.js
+++ Workshop Soft Wiki/app.js
@@ -4,6 +4,7 @@ import layot from './views/layout.js';
 import home from './views/home.js';
 import login from './views/login.js'
 import register from './views/register.js';
+import notFound from './views/not-found.js';
 
 
 const routes = [
@@ -19,11 +20,15 @@ const routes = [
         path: '/register',
         template: register
     },
+    {
+        path: '/not-found',
+        template: notFound
+    }
 ];
 
 const router = (path) => {
 
-    let rout = routes.find(x => x.path === path);
+    let rout = routes.find(x => x.path === path) || routes.find(x => x.path === '/not-found');
     render(layot(rout.template(), { navigationHandler }), document.getElementById('app'));
 
 };
---
+++ Workshop Soft Wiki/views/not-found.js
@@ -0,0 +1,5 @@
+import {html, render} from '../node_modules/lit-html/lit-html.js';
+
+export default ()=>html`  
+    <h1 style="color:white">The page is not found</h1>
+`;
